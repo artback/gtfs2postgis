9f4982c85cc287c0d611093b1202a88d7235e653
Artback <artback@openmailbox.org>
2020-04-14 15:02:17 +0200

    Add a docker file

--- filehandling/fileHandler.go
+++ filehandling/fileHandler.go
@@ -1,84 +1,14 @@
-package main
+package filehandling
 
 import (
 	"archive/zip"
 	"fmt"
-	"github.com/artback/gtfs2postgis/config"
-	"github.com/artback/gtfs2postgis/query"
 	"io"
 	"net/http"
 	"os"
-	"path"
 	"path/filepath"
-	"runtime"
 	"strings"
 )
-
-var (
-	conf *config.Configuration
-	repo *query.Repository
-)
-
-func init() {
-	conf = new(config.Configuration)
-	repo = new(query.Repository)
-
-	err := config.Init(conf)
-	if err != nil {
-		panic(err)
-	}
-}
-
-func main() {
-	_, filename, _, ok := runtime.Caller(0)
-	if !ok {
-		panic("No caller information")
-	}
-	dir := path.Dir(filename)
-
-	if err := DownloadFile("gtfs.zip", conf.Host.Url); err != nil {
-		panic(err)
-	}
-	Unzip("gtfs.zip", "./gtfs")
-
-	err := repo.Connect(conf.Database)
-	if err != nil {
-		panic(err)
-	}
-
-	err = repo.PopulateTable("agency", fmt.Sprintf("%s/gtfs/agency.txt", dir))
-	if err != nil {
-		panic(err)
-	}
-	err = repo.PopulateTable("calendar_dates", fmt.Sprintf("%s/gtfs/calendar_dates.txt", dir))
-	if err != nil {
-		panic(err)
-	}
-	err = repo.PopulateTable("routes", fmt.Sprintf("%s/gtfs/routes.txt", dir))
-	if err != nil {
-		panic(err)
-	}
-	err = repo.PopulateTable("stops", fmt.Sprintf("%s/gtfs/stops.txt", dir))
-	if err != nil {
-		panic(err)
-	}
-
-	err = repo.PopulateTable("trips", fmt.Sprintf("%s/gtfs/trips.txt", dir))
-	if err != nil {
-		panic(err)
-	}
-
-	err = repo.PopulateTable("stop_times", fmt.Sprintf("%s/gtfs/stop_times.txt", dir))
-	if err != nil {
-		panic(err)
-	}
-	err = os.RemoveAll("./gtfs")
-	err = os.Remove("./gtfs.zip")
-	if err != nil {
-		panic(err)
-	}
-
-}
 
 func DownloadFile(filepath string, url string) error {
 
